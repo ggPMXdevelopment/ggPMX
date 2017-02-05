f81b405da47017f21ed993e088eff5b70d826fb5
ludwiin1 <inga.ludwig@novartis.com>
2017-02-05 16:51:04 +0100

    Added unit tests, test data, standard configurations and a working vignette

---
+++ tests/testthat/test-gpar.R
@@ -0,0 +1,16 @@
+context("Test graphical parameters object")
+
+
+test_that("pmx_gpar defaults are well setted", {
+ gpars <- ggPMX::pmx_gpar()
+ expect_equal("")
+})
+
+## test get/subsetting
+
+## test set multiple parameters
+
+## set a parameter within a list of others parameters
+
+
+
---
+++ tests/testthat/test-reader.R
@@ -0,0 +1,8 @@
+
+
+context("Test reader parameters")
+
+
+test_that("laod standing config", {
+  load_config("standing","mlx")
+})
