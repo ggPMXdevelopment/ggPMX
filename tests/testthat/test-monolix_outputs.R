0221d5a50d0b395a0f35b5a0d4e69373aa8b6efe
amingassem <amine.gassem@ag-study.com>
2017-03-01 15:28:31 -0800

    Try initiating controller and IND plot from RESULTS dirs

--- tests/testthat/test-monolix_outputs.R
+++ tests/testthat/test-monolix_outputs.R
@@ -1,2 +1,22 @@
 context("monolix_outputs")
 
+monolix_data <- source_monolix_data()
+
+for(i in 1:length(monolix_data)){
+  # define ctr
+  testthat::test_that(
+    paste0("canSetControllerFor", names(monolix_data[i])), {
+      ctr <- pmx(config = "standing", sys = "mlx", 
+                 directory = monolix_data[[i]])
+    })
+  
+  testthat::test_that(
+    paste0("canPlotINDfor", names(monolix_data[i])), 
+    {
+      plotName <- paste0("testIND", names(monolix_data[i]))
+      ctr %>% set_plot(ptype="IND", pname = plotName)
+      ctr %>% get_plot(plotName)
+    }
+  )
+  
+}
