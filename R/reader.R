530d8ac0cdd9a474a0d4a1821e7c97518de9a33d
amingassem <amine.gassem@ag-study.com>
2017-03-03 05:20:47 -0800

    Allow for regepx matching on the column names in prediction.txt

--- R/reader.R
+++ R/reader.R
@@ -2,11 +2,11 @@ read_mlx_ind_est <- function(path,x){
   ds <- fread(path)
   ds <- ds[,!grep("^V[0-9]+",names(ds)),with=FALSE]
   setnames(ds,tolower(names(ds)))
-
+  
   ds <- melt(ds,measure=grep("_.*_",names(ds)))
   setnames(ds,toupper(gsub("_|[0-9]+","",names(ds))))
   ds[,c("VAR","EFFECT","FUN"):=tstrsplit(VARIABLE,"_")]
-
+  
 }
 
 #' Read MONOLIX model predictions
@@ -24,10 +24,15 @@ read_mlx_pred <- function(path,x){
   col_stars <- grep("*",names(xx),fixed=TRUE,value=TRUE)
   ncol_stars <- toupper(gsub("ind","I",gsub("_.*","",col_stars)))
   nn <- c(x$names,setNames(ncol_stars,col_stars))
-  nn <- nn[names(nn)%in%names(xx)]
-
-  setnames(xx,names(nn),as.character(nn))
-  xx[,as.character(nn),with=FALSE]
+  match_names <- vapply(names(nn), 
+                        function(x){
+                          out <- which(grepl(x, names(xx)))
+                          if(length(out) == 0L){NA}else{out}
+                        }, 
+                        integer(1), USE.NAMES = FALSE)
+  setnames(xx,names(xx)[match_names[!is.na(match_names)]],
+           as.character(nn[!is.na(match_names)]))
+  xx[,as.character(nn[!is.na(match_names)]),with=FALSE]
 }
 
 
@@ -65,8 +70,8 @@ load_data_set <- function(x,path,sys){
     cat(sprintf(" %s FILE DO NOT exist under %s",x[["file"]],path))
     return(NULL)
   }
-
-
+  
+  
   if(exists("reader",x))
     return(do.call(x[["reader"]],list(fpath,x)))
   ds <- fread(fpath)
@@ -94,7 +99,7 @@ load_source <- function(sys,path,dconf,include,exclude){
   names. <- names(dconf)
   if(!missing(include)) names. <- include
   if(!missing(exclude)) names. <- setdiff(names.,exclude)
-
+  
   datasets <- dconf[names.]
   dxs <- lapply(datasets,
                 load_data_set,
@@ -107,15 +112,15 @@ load_source <- function(sys,path,dconf,include,exclude){
 post_load <- function(dxs,sys,dplot){
   mm <- unlist(dplot)
   if(sys=="mlx"){
-   ## add startification column
-       vv <- names(dxs$ind_pred)[vapply(dxs$ind_pred,is.integer,T)]
-      ## prepare data set for stratification
-      dxs$mod_pred <-
-        merge(dxs$mod_pred,
-              unique(dxs$ind_pred[,vv,with=FALSE]),
-              by="ID")
-   ## add shrinkage data set
-   dxs[["shrink"]] <- shrinkage(dxs[["par_est"]],dxs[["ind_pred"]],sys = sys)
+    ## add startification column
+    vv <- names(dxs$ind_pred)[vapply(dxs$ind_pred,is.integer,T)]
+    ## prepare data set for stratification
+    dxs$mod_pred <-
+      merge(dxs$mod_pred,
+            unique(dxs$ind_pred[,vv,with=FALSE]),
+            by="ID")
+    ## add shrinkage data set
+    dxs[["shrink"]] <- shrinkage(dxs[["par_est"]],dxs[["ind_pred"]],sys = sys)
   }
   dxs
 }
