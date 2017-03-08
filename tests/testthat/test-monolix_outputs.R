8c5d480d2a3e91247a7deae416b53fbb9dd670fe
amingassem <amine.gassem@ag-study.com>
2017-03-08 01:30:10 -0800

    Update monolix test to firstly only test PK data

--- tests/testthat/test-monolix_outputs.R
+++ tests/testthat/test-monolix_outputs.R
@@ -2,18 +2,18 @@ context("monolix_outputs")
 
 monolix_data <- source_monolix_data()
 
-for(i in 1:length(monolix_data)){
+for(i in 1:length(monolix_data[["PK"]])){
   # define ctr
   testthat::test_that(
-    paste0("canSetControllerFor", names(monolix_data[i])), {
+    paste0("canSetControllerFor", names(monolix_data[["PK"]][i])), {
       ctr <- pmx(config = "standing", sys = "mlx", 
-                 directory = monolix_data[[i]])
+                 directory = monolix_data[["PK"]][[i]])
     })
   
   testthat::test_that(
-    paste0("canPlotINDfor", names(monolix_data[i])), 
+    paste0("canPlotINDfor", names(monolix_data[["PK"]][i])), 
     {
-      plotName <- paste0("testIND", names(monolix_data[i]))
+      plotName <- paste0("testIND", names(monolix_data[["PK"]][i]))
       ctr %>% set_plot(ptype="IND", pname = plotName)
       ctr %>% get_plot(plotName)
     }
