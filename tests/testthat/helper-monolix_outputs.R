5718361c2be420268bc59e8c8d1067cc06bc6317
amingassem <amine.gassem@ag-study.com>
2017-03-08 01:17:55 -0800

    Split monolix data into Pk/PKPD

--- tests/testthat/helper-monolix_outputs.R
+++ tests/testthat/helper-monolix_outputs.R
@@ -4,15 +4,22 @@ source_monolix_data <- function(){
     stop("Please set an environment variable PMXTESTDATA containing", 
          " monolix RESULTS directories")
   }
-  result_dirs <- grep("RESULTS", 
-                      list.dirs("~/pmx_data", full.names = TRUE, 
-                                recursive = TRUE), 
+  result_dirs_PKPD <- grep("RESULTS", 
+                      list.dirs(file.path(monolix_dir, "PKPD"),
+                                full.names = TRUE), 
                       value = TRUE)
-  if(length(result_dirs) == 0){
+  result_dirs_PK <- grep("RESULTS", 
+                           list.dirs(monolix_dir, full.names = TRUE), 
+                           value = TRUE)
+  result_dirs_PK <- result_dirs_PK[!result_dirs_PK %in% result_dirs_PKPD]
+  if(length(c(result_dirs_PK, result_dirs_PKPD)) == 0){
     stop(monolix_dir, " contains no RESULTS directories")
   }
-  results_labels <- vapply(result_dirs, function(x){
-    basename(dirname(x))
-  }, character(1), USE.NAMES = FALSE)
-  setNames(result_dirs, results_labels)
+  label_dirs <- function(result_dirs){
+    results_labels <- vapply(result_dirs, function(x){
+      basename(dirname(x))
+    }, character(1), USE.NAMES = FALSE)
+    setNames(result_dirs, results_labels)
+  }
+  lapply(list(PK = result_dirs_PK, PKPD = result_dirs_PKPD), label_dirs)
 }
