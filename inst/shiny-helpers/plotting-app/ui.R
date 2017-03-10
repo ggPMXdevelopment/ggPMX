2eb3a8482538c5b52c45e293cbbaac3004f5759e
amingassem <amine.gassem@ag-study.com>
2017-03-10 13:30:09 -0800

    Add a monolix input

--- inst/shiny-helpers/plotting-app/ui.R
+++ inst/shiny-helpers/plotting-app/ui.R
@@ -1,3 +1,13 @@
+monolixInput <- function(id) {
+  # Create a namespace function using the provided id
+  ns <- NS(id)
+  
+  tagList(
+    selectInput(ns("mlpath"), "Choose a dataset:", 
+                as.list(monolix_data[["PK"]]))
+  )
+}
+
 fluidPage(
   
   # Application title
@@ -7,7 +17,7 @@ fluidPage(
     
     # Sidebar with a slider input
     sidebarPanel(
-      csvFileInput("datafile", "User data (.csv format)")
+      monolixInput("resultsPath")
     ),
     
     # Show a plot of the generated distribution
