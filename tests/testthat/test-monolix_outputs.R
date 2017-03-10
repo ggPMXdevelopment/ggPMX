07e5130fefffc0c4024ce12013fd3988c06179d5
amingassem <amine.gassem@ag-study.com>
2017-03-10 07:45:48 -0800

    Add tests for residuals. NPD vs. EPRED, NPD vs. TIME, IWRES vs. IPRED, and IWRES vs. TIME

--- tests/testthat/test-monolix_outputs.R
+++ tests/testthat/test-monolix_outputs.R
@@ -4,40 +4,75 @@ monolix_data <- source_monolix_data()
 
 for(i in 1:length(monolix_data[["PK"]])){
   pmxOptions(work_dir = monolix_data[["PK"]][[i]])
+  resName <- names(monolix_data[["PK"]][i])
   # define ctr
   testthat::test_that(
-    paste0("canSetControllerFor", names(monolix_data[["PK"]][i])), {
+    paste0("canSetControllerFor", resName), {
       ctr <- pmx_mlx("standing")
     })
   
   testthat::test_that(
-    paste0("canPlotINDfor", names(monolix_data[["PK"]][i])), 
+    paste0("canPlotINDfor", resName), 
     {
       ctr <- pmx_mlx("standing")
-      plotName <- paste0("testIND", names(monolix_data[["PK"]][i]))
+      plotName <- paste0("testIND", resName)
       ctr %>% set_plot(ptype="IND", pname = plotName)
       ctr %>% get_plot(plotName)
     }
   )
 
   testthat::test_that(
-    paste0("canPlotDISfor", names(monolix_data[["PK"]][i])), 
+    paste0("canPlotDISfor", resName), 
     {
       ctr <- pmx_mlx("standing")
-      plotName <- paste0("testDIS", names(monolix_data[["PK"]][i]))
+      plotName <- paste0("testDIS", resName)
       ctr %>% set_plot(ptype="DIS", pname = plotName)
       ctr %>% get_plot(plotName)
     }
   )
 
   testthat::test_that(
-    paste0("canPlotRESfor", names(monolix_data[["PK"]][i])), 
+    paste0("canPlotRESfor", resName), 
     {
-      ctr <- pmx(config = "standing", sys = "mlx", 
-                 directory = monolix_data[["PK"]][[i]])
-      plotName <- paste0("testRES", names(monolix_data[["PK"]][i]))
-      ctr %>% set_plot(ptype="RES", pname = plotName, x = )
-      ctr %>% get_plot(plotName)
+      ctr <- pmx_mlx("standing")
+      # check plots
+      appVars <- names(ctr[["data"]][["mod_pred"]])
+      # NPD versus EPRED
+      testVars <- c("NPD", "EPRED")
+      if(all(testVars %in% appVars)){
+        plotName <- paste0("testRESx=", testVars[2], "y=", testVars[1],
+                           names(monolix_data[["PK"]][i]))
+        ctr %>% set_plot(ptype="RES", pname = plotName, 
+                         x = testVars[2], y = testVars[1])
+        ctr %>% get_plot(plotName)
+      }
+      # NPD versus TIME
+      testVars <- c("NPD", "TIME")
+      if(all(testVars %in% appVars)){
+        plotName <- paste0("testRESx=", testVars[2], "y=", testVars[1],
+                           names(monolix_data[["PK"]][i]))
+        ctr %>% set_plot(ptype="RES", pname = plotName, 
+                         x = testVars[2], y = testVars[1])
+        ctr %>% get_plot(plotName)
+      }
+      # IWRES versus IPRED
+      testVars <- c("IWRES", "IPRED")
+      if(all(testVars %in% appVars)){
+        plotName <- paste0("testRESx=", testVars[2], "y=", testVars[1],
+                           names(monolix_data[["PK"]][i]))
+        ctr %>% set_plot(ptype="RES", pname = plotName, 
+                         x = testVars[2], y = testVars[1])
+        ctr %>% get_plot(plotName)
+      }
+      # IWRES versus TIME
+      testVars <- c("IWRES", "TIME")
+      if(all(testVars %in% appVars)){
+        plotName <- paste0("testRESx=", testVars[2], "y=", testVars[1],
+                           names(monolix_data[["PK"]][i]))
+        ctr %>% set_plot(ptype="RES", pname = plotName, 
+                         x = testVars[2], y = testVars[1])
+        ctr %>% get_plot(plotName)
+      }
     }
   )
   
