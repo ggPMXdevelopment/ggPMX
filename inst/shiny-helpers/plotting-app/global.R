742a38e88cc96035f2fde9d3f60cbd21b8bf4549
amingassem <amine.gassem@ag-study.com>
2017-03-10 11:34:24 -0800

    Pad out global.R

---
+++ inst/shiny-helpers/plotting-app/global.R
@@ -0,0 +1,29 @@
+library(shiny)
+library(ggPMX)
+source_monolix_data <- function(){
+  monolix_dir <- Sys.getenv("PMXTESTDATA")
+  if(identical(monolix_dir, "")){
+    stop("Please set an environment variable PMXTESTDATA containing", 
+         " monolix RESULTS directories")
+  }
+  result_dirs_PKPD <- grep("RESULTS", 
+                           list.dirs(file.path(monolix_dir, "PKPD"),
+                                     full.names = TRUE), 
+                           value = TRUE)
+  result_dirs_PK <- grep("RESULTS", 
+                         list.dirs(monolix_dir, full.names = TRUE), 
+                         value = TRUE)
+  result_dirs_PK <- result_dirs_PK[!result_dirs_PK %in% result_dirs_PKPD]
+  if(length(c(result_dirs_PK, result_dirs_PKPD)) == 0){
+    stop(monolix_dir, " contains no RESULTS directories")
+  }
+  label_dirs <- function(result_dirs){
+    results_labels <- vapply(result_dirs, function(x){
+      basename(dirname(x))
+    }, character(1), USE.NAMES = FALSE)
+    setNames(result_dirs, results_labels)
+  }
+  lapply(list(PK = result_dirs_PK, PKPD = result_dirs_PKPD), label_dirs)
+}
+
+monolix_data <- source_monolix_data()
