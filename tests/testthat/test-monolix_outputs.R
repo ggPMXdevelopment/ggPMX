9baf2cca66f627b679b992160e91333cad13d9ae
amingassem <amine.gassem@ag-study.com>
2017-03-10 06:24:11 -0800

    Use pmxOptions to set working directory

--- tests/testthat/test-monolix_outputs.R
+++ tests/testthat/test-monolix_outputs.R
@@ -3,18 +3,17 @@ context("monolix_outputs")
 monolix_data <- source_monolix_data()
 
 for(i in 1:length(monolix_data[["PK"]])){
+  pmxOptions(work_dir = monolix_data[["PK"]][[i]])
   # define ctr
   testthat::test_that(
     paste0("canSetControllerFor", names(monolix_data[["PK"]][i])), {
-      ctr <- pmx(config = "standing", sys = "mlx", 
-                 directory = monolix_data[["PK"]][[i]])
+      ctr <- pmx_mlx("standing")
     })
   
   testthat::test_that(
     paste0("canPlotINDfor", names(monolix_data[["PK"]][i])), 
     {
-      ctr <- pmx(config = "standing", sys = "mlx", 
-                 directory = monolix_data[["PK"]][[i]])
+      ctr <- pmx_mlx("standing")
       plotName <- paste0("testIND", names(monolix_data[["PK"]][i]))
       ctr %>% set_plot(ptype="IND", pname = plotName)
       ctr %>% get_plot(plotName)
@@ -24,8 +23,7 @@ for(i in 1:length(monolix_data[["PK"]])){
   testthat::test_that(
     paste0("canPlotDISfor", names(monolix_data[["PK"]][i])), 
     {
-      ctr <- pmx(config = "standing", sys = "mlx", 
-                 directory = monolix_data[["PK"]][[i]])
+      ctr <- pmx_mlx("standing")
       plotName <- paste0("testDIS", names(monolix_data[["PK"]][i]))
       ctr %>% set_plot(ptype="DIS", pname = plotName)
       ctr %>% get_plot(plotName)
