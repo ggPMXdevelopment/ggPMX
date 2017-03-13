755e3b05c65ffc7546e5201e85ed87efb4fd2c14
amingassem <amine.gassem@ag-study.com>
2017-03-13 10:42:24 -0700

    Tidy up server side

--- inst/shiny-helpers/plotting-app/server.R
+++ inst/shiny-helpers/plotting-app/server.R
@@ -6,15 +6,27 @@ function(input, output, session) {
     input$mlpath
   })
   
+  plottype <- reactive({
+    validate(need(input$plttype, "Please choose a plot type"))
+    input$plttype
+  })
+  
   # The controller
   observe({
     pmxOptions(work_dir = userdirectory())
-    ctr <- pmx_mlx("standing")
+    ctr <<- pmx_mlx("standing")
   }, priority = 100L)
   
+  output$plottypes <- renderUI({
+    userdirectory()
+    selectInput("plttype", "Choose a plottype:", 
+                as.list(ctr$plots()))
+  })
+  
   output$plot <- renderPlot({
-    ctr %>% get_plot("indiv")
+    validate(need(input$mlpath, "Please choose a dataset"), 
+             need(input$plttype, "Please choose a plot type"))
+    ctr %>% get_plot(plottype())
   }
   )
-  
 }
