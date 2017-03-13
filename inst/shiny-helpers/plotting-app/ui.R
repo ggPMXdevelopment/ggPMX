0963a1ffd4f1777fb5e904ca8feffac6d4f2c37b
amingassem <amine.gassem@ag-study.com>
2017-03-13 12:39:47 -0700

    Change UI look. Begin adding graphical options

--- inst/shiny-helpers/plotting-app/ui.R
+++ inst/shiny-helpers/plotting-app/ui.R
@@ -2,21 +2,38 @@ fluidPage(
   
   # Application title
   titlePanel("ggPMX Plot Tester"),
-  
-  sidebarLayout(
-    
-    # Sidebar with a slider input
-    sidebarPanel(
-      tagList(
-        selectInput("mlpath", "Choose a dataset:", 
-                    as.list(monolix_data[["PK"]])), 
-        uiOutput("plottypes")
-      )
+  fluidRow(
+    column(3,
+           tagList(
+             selectInput("mlpath", "Choose a dataset:", 
+                         as.list(monolix_data[["PK"]])), 
+             uiOutput("plottypes")
+           )
     ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("plot")
+    column(9,plotOutput("plot"))
+  ), 
+  h4("Graphical Options"),
+  fluidRow(
+    wellPanel(
+      column(3,
+             h4("Diamonds Explorer"),
+             sliderInput('sampleSize', 'Sample Size', 
+                         min=1, max=nrow(dataset), value=min(1000, nrow(dataset)), 
+                         step=500, round=0),
+             br(),
+             checkboxInput('jitter', 'Jitter'),
+             checkboxInput('smooth', 'Smooth')
+      ),
+      column(4, offset = 1,
+             selectInput('x', 'X', names(dataset)),
+             selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
+             selectInput('color', 'Color', c('None', names(dataset)))
+      ),
+      column(4,
+             selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
+             selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
+      )
     )
   )
+  
 )
