fb246c542356ef565f11b6827afcb3acfe729bb3
amingassem <amine.gassem@ag-study.com>
2017-03-14 10:44:55 -0700

    Start moving graphical inputs to tab panels

--- inst/shiny-helpers/plotting-app/ui.R
+++ inst/shiny-helpers/plotting-app/ui.R
@@ -14,52 +14,53 @@ fluidPage(
   ), 
   h4("Graphical Options"),
   fluidRow(
-    wellPanel(
-      column(3,
-             uiOutput("labels")
-      ),
-      column(3,
-             h4("Has Smooth options"),
-             checkboxInput("hassmooth", label = "Has smooth?", 
-                           value = FALSE),
-             conditionalPanel(
-               "input.hassmooth == true",
-               checkboxInput("hassmoothse", label = "SE?", 
+    tabsetPanel(
+      tabPanel("Labels", uiOutput("labels")),
+      tabPanel(
+        "Misc", 
+        column(3,
+               h4("Has Smooth options"),
+               checkboxInput("hassmooth", label = "Has smooth?", 
                              value = FALSE),
-               numericInput("hassmoothlinetype", "Line Type", 
-                            value = 2L),
-               numericInput("hassmoothsize", "Size", 
-                            value = 0.5, step = 0.1)
-             )
-      ),
-      column(3,
-             h4("Has Band"),
-             checkboxInput("hasband", label = "Has band?", 
-                           value = FALSE),
-             conditionalPanel(
-               "input.hasband == true",
-               sliderInput("hasbandband", label = "Band", 
+               conditionalPanel(
+                 "input.hassmooth == true",
+                 checkboxInput("hassmoothse", label = "SE?", 
+                               value = FALSE),
+                 numericInput("hassmoothlinetype", "Line Type", 
+                              value = 2L),
+                 numericInput("hassmoothsize", "Size", 
+                              value = 0.5, step = 0.1)
+               )
+        ),
+        column(3,
+               h4("Has Band"),
+               checkboxInput("hasband", label = "Has band?", 
+                             value = FALSE),
+               conditionalPanel(
+                 "input.hasband == true",
+                 sliderInput("hasbandband", label = "Band", 
                              step = 1L, value = c(-2L, 2L), 
-                           min = -10L, max = 10L),
-               numericInput("hasbandlinetype", "Line Type", 
-                            value = 2L),
-               numericInput("hasbandsize", "Size", 
-                            value = 0.5, step = 0.1)
-             )
-      ),
-      column(3,
-             h4("Is Draft"),
-             checkboxInput("isdraft", label = "Is Draft?", 
-                           value = TRUE),
-             conditionalPanel(
-               "input.isdraft == true",
-               numericInput("isdraftsize", "Size", 
-                            value = 20L, step = 1L),
-               textInput("isdraftlabel", "Label", "DRAFT"),
-               textInput("isdraftcolor", "Color", "grey50")
-             )
+                             min = -10L, max = 10L),
+                 numericInput("hasbandlinetype", "Line Type", 
+                              value = 2L),
+                 numericInput("hasbandsize", "Size", 
+                              value = 0.5, step = 0.1)
+               )
+        ),
+        column(3,
+               h4("Is Draft"),
+               checkboxInput("isdraft", label = "Is Draft?", 
+                             value = TRUE),
+               conditionalPanel(
+                 "input.isdraft == true",
+                 numericInput("isdraftsize", "Size", 
+                              value = 20L, step = 1L),
+                 textInput("isdraftlabel", "Label", "DRAFT"),
+                 textInput("isdraftcolor", "Color", "grey50")
+               )
+        )
       )
     )
+    
   )
-  
 )
