0d5a5a48f1c05d05459e7628cf0c37430b1c872f
amingassem <amine.gassem@ag-study.com>
2017-03-16 12:45:12 -0700

    Begin testing pmx options

---
+++ tests/testthat/test-pmx_options.R
@@ -0,0 +1,11 @@
+context("Test pmx options")
+
+test_that("can get pmx options", {
+  default_options <- pmxOptions()
+  expect_identical(default_options$work_dir, "/home/agstudy")
+})
+
+test_that("can set option", {
+  pmxOptions(myOption = 10L)
+  expect_identical(getPmxOption("myOption"), 10L)
+})
