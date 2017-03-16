a80b80bb4fe5bef0049d49ca2187e034c0a6b87d
amingassem <amine.gassem@ag-study.com>
2017-03-16 13:09:48 -0700

    Add tests for startup options

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onLoad <- function(libname = find.package("ggPMX"), pkgname = "ggPMX"){
 
-  pmxOptions(work_dir="/home/agstudy")
+  pmxOptions(work_dir = "/home/agstudy")
 
-  pmxOptions(template_dir=file.path(libname,pkgname,"templates"))
+  pmxOptions(template_dir = file.path(libname, pkgname, "templates"))
 
 
 }
