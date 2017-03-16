8c4e7d399ade05598b1737fdb142b63bce5cf384
amingassem <amine.gassem@ag-study.com>
2017-03-16 12:26:51 -0700

    Test printing config

--- tests/testthat/test-config.R
+++ tests/testthat/test-config.R
@@ -4,3 +4,8 @@ test_that("can source configs", {
   conf <- configs()
   expect_identical(conf$name, c("config1", "standing"))
 })
+
+test_that("can print configs", {
+  conf <- configs()
+  expect_output(print(conf), "There are 2 configs for mlx system ")
+})
