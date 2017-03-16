ab66be1e0a04a1b07f0aadf19944bf356e94d200
amingassem <amine.gassem@ag-study.com>
2017-03-16 12:31:05 -0700

    test can load and print config

--- tests/testthat/test-config.R
+++ tests/testthat/test-config.R
@@ -9,3 +9,16 @@ test_that("can print configs", {
   conf <- configs()
   expect_output(print(conf), "There are 2 configs for mlx system ")
 })
+
+
+test_that("can load configs", {
+  conf <- configs()
+  cfig <- load_config(conf$name[1], "mlx")
+  expect_s3_class(cfig, "pmxConfig")
+})
+
+test_that("can print loaded config", {
+  conf <- configs()
+  cfig <- load_config(conf$name[1], "mlx")
+  expect_output(print(cfig), "configuration Object")
+})
