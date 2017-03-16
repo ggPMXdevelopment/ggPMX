6d15ef29268cc823baca5f9971bea230b3789e80
amingassem <amine.gassem@ag-study.com>
2017-03-16 04:29:55 -0700

    Add test that data sets can be loaded

--- tests/testthat/test-reader.R
+++ tests/testthat/test-reader.R
@@ -2,8 +2,28 @@ context("Test reader parameters")
 reader_help <- reader_helpers()
 
 test_that("load standing config", {
-  expect_silent(load_config("standing","mlx"))
-  expect_indentical(reader_help$conf$sys, "mlx")
+  expect_silent(load_config("standing", "mlx"))
+  expect_identical(reader_help$conf$sys, "mlx")
+})
+
+
+test_that("can load data set", {
+  names. <- names(reader_help$conf$data)
+  datasets <- reader_help$conf$data[names.]
+  path <- reader_help$wd
+  sys <- reader_help$conf$sys
+  dxs <- lapply(datasets,
+                load_data_set,
+                path = path,
+                sys = sys)
+  expect_identical(names(dxs$par_est), 
+                   c("PARAM", "VALUE", "SE", "RSE", "PVALUE"))
+  expect_identical(names(dxs$mod_pred), 
+                   c("ID", "TIME", "DV", "PRED", "NPDE", "IPRED", "IWRES"))
+  expect_identical(names(dxs$ind_pred), 
+                   c("ID", "EVID", "TWT", "TAGE", "SEX", "STUD", 
+                     "VARIABLE", "VALUE", 
+                     "VAR", "EFFECT", "FUN"))
 })
 
 test_that("can read mlx ind", {
