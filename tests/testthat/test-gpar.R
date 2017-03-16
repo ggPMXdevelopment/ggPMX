5c3ae2f681c5a3fa2bbc3c321cc2f537e41390cc
amingassem <amine.gassem@ag-study.com>
2017-03-16 13:41:38 -0700

    Add some tests for gpar.R

--- tests/testthat/test-gpar.R
+++ tests/testthat/test-gpar.R
@@ -1,18 +1,24 @@
 context("Test graphical parameters object")
 
-
 test_that("pmx_gpar defaults are well setted", {
-  # This test currently breaks
-  # TODO: Make it run again
- #gpars <- ggPMX::pmx_gpar()
- #expect_equal("")
+  gpars <- ggPMX::pmx_gpar(labels = list(title = "hello"))
+  expect_identical(
+    gpars$draft, 
+    list(size = 20, label = "DRAFT", color = "grey50")
+  )
+  expect_identical(
+    gpars$smooth, 
+    list(se = FALSE, linetype = 2, size = 1.5, method = "loess")
+  )
+  
 })
 
-## test get/subsetting
+test_that("can test object is pmx_gpar", {
+  gpars <- ggPMX::pmx_gpar(labels = list(title = "hello"))
+  expect_true(is.pmx_gpar(gpars))
+})
 
-## test set multiple parameters
-
-## set a parameter within a list of others parameters
-
-
-
+test_that("can print pmx_gpar object", {
+  gpars <- ggPMX::pmx_gpar(labels = list(title = "hello"))
+  expect_output(print(gpars), "\"hello\"")
+})
