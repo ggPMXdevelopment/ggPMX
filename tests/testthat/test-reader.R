bb5aa543258eb4643b97fab5da8c4aa2eafeac58
amingassem <amine.gassem@ag-study.com>
2017-03-16 04:17:15 -0700

    Add tests for loading standing config

--- tests/testthat/test-reader.R
+++ tests/testthat/test-reader.R
@@ -1,8 +1,12 @@
-
-
 context("Test reader parameters")
+reader_help <- reader_helpers()
 
-
-test_that("laod standing config", {
-  load_config("standing","mlx")
+test_that("load standing config", {
+  expect_silent(load_config("standing","mlx"))
+  expect_indentical(reader_help$conf$sys, "mlx")
 })
+
+test_that("can read mlx ind", {
+  
+})
+
