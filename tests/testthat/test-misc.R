32858377c15d704cfba5e6185ac982d1eb124ec3
agstudy <contact@ag-study.com>
2017-03-20 13:23:32 +0100

    delete scientific tests files.

--- tests/testthat/test-misc.R
+++
@@ -1,9 +0,0 @@
-# Unit tests example, part 3
-# --------------------------
-#
-# ADAPT THIS FILE FOR YOUR PROJECT AS NEEDED (OR DELETE IF UNUSED).
-#
-# This is the template for additional tests of funtionality beyond that 
-# described in the vignette.
-#
-# See test-examples.R for more details on how units tests should be structured.
