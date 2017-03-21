2ac0a66db0d60ba492891cd89c46217936bd5cf0
gasseam1 <amine.gassem@ag-study.com>
2017-03-21 09:13:36 -0700

    test can update DIS plots

--- tests/testthat/test-update_plots.R
+++ tests/testthat/test-update_plots.R
@@ -6,6 +6,19 @@ test_that("can update DIS plot", {
   ctr %>% set_plot("DIS", pname = "distr1", type = "box")
   expect_true("distr1" %in% ctr$plots())
   p <- ctr %>% get_plot("distr1")
+  oldconf <- ctr$get_config("distr1")
+  expect_true(oldconf$has.shrink)
+  shrink <- data.frame(EFFECT = c("cl", "ka", "v"), 
+                       SHRINK = c(0.1, 0.2, 0.7), 
+                       stringsAsFactors = FALSE)
+  ctr %>% update_plot(
+    "distr1",
+    has.shrink = TRUE,
+    shrink = shrink
+  )
+  newconf <- ctr$get_config("distr1")
+  expect_identical(newconf$shrink, as.list(shrink))
+  
 })
 
 test_that("can remove DIS plot", {
