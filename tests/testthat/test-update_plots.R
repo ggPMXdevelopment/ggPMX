8313c8bf15ceef4a3957290e3430aca3384fccc7
gasseam1 <amine.gassem@ag-study.com>
2017-03-21 07:23:06 -0700

    Add update_plot tests

--- tests/testthat/test-update_plots.R
+++ tests/testthat/test-update_plots.R
@@ -1,5 +1,15 @@
 context("Test update plots")
+helpers <- helper_updateplots()
 
 test_that("can update DIS plot", {
-  
+  ctr <- helpers$ctr
+  ctr %>% set_plot("DIS", pname = "distr1", type = "box")
+  expect_true("distr1" %in% ctr$plots())
+  p <- ctr %>% get_plot("distr1")
+})
+
+test_that("can remove DIS plot", {
+  ctr <- helpers$ctr
+  ctr$remove_plot("distr1")
+  expect_false("distr1" %in% ctr$plots())
 })
