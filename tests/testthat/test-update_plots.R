93c149911b94ea101f6370545fbfb85d424f2f18
gasseam1 <amine.gassem@ag-study.com>
2017-03-21 09:34:54 -0700

    Add IND plot update tests

--- tests/testthat/test-update_plots.R
+++ tests/testthat/test-update_plots.R
@@ -30,5 +30,19 @@ test_that("can remove DIS plot", {
 test_that("can update IND plot", {
   ctr <- helpers$ctr
   ctr %>% set_plot("IND", pname = "indiv1")
+  expect_silent(ctr %>% get_plot("indiv1", c(2,4)))
   expect_true("indiv1" %in% ctr$plots())
+  oldconf <- ctr$get_config("indiv1")
+  expect_true(oldconf$gp$has.band)
+  
+  ctr %>% update_plot("indiv1", has.band = FALSE)
+  newconf <- ctr$get_config("indiv1")
+  expect_false(newconf$gp$has.band)
 })
+
+test_that("can remove IND plot", {
+  ctr <- helpers$ctr
+  ctr$remove_plot("indiv1")
+  expect_false("indiv1" %in% ctr$plots())
+})
+
