fb1748fb1e98e65f39684eac2c70a97d82f4b6b6
gasseam1 <amine.gassem@ag-study.com>
2017-03-22 11:33:22 -0700

    Add additional assertions

--- R/assertions.R
+++ R/assertions.R
@@ -1,7 +1,32 @@
+is_string <- function(x) {
+  is.character(x) && length(x) == 1 && !is.na(x)
+}
+
+assertthat::on_failure(is_string) <-  function(call, env) {
+  paste0(deparse(call$x), " is not a string")
+}
+
 is_pmx_gpar <- function(x) {
   is.pmx_gpar(x)
 }
 
 assertthat::on_failure(is_pmx_gpar) <-  function(call, env) {
-  paste0(deparse(call$x), " is not a pmx_gpar object")
+  paste0(deparse(call$x), " is not an object of class 'pmx_gpar'.")
 }
+
+is_configs <- function(x) {
+  inherits(x, "configs")
+}
+
+assertthat::on_failure(is_configs) <-  function(call, env) {
+  paste0(deparse(call$x), " is not an object of class 'configs'.")
+}
+
+is_pmxconfig <- function(x) {
+  inherits(x, "pmxConfig")
+}
+
+assertthat::on_failure(is_configs) <-  function(call, env) {
+  paste0(deparse(call$x), " is not an object of class 'pmxConfig'.")
+}
+
