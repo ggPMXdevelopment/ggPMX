12259bdbdd5f9c1aa81b4c54aec605fd4a314a8d
gasseam1 <amine.gassem@ag-study.com>
2017-03-24 15:11:39 +0100

    Add pmx_filter function body

--- R/pmx_filter.R
+++ R/pmx_filter.R
@@ -6,6 +6,14 @@
 #'
 #' @return Returns a pmx controller with a filtered data set.
 #' @export
-pmx_filter <- function(ctr, data_set, expression){
-  assert_that(is_pmxclass(ctr))
-}
+pmx_filter <- 
+  function(ctr, data_set =c("par_est","mod_pred", 
+                            "ind_pred", "shrink"), expression){
+    assert_that(is_pmxclass(ctr))
+    data_set <- match.arg(data_set)
+    assert_that(is.expression_or_string(expression))
+    oldData <- ctr[["data"]]
+    newData <- ctr[["data"]][[data_set]][, expression]
+    ctr$data <- newData
+    ctr
+  }
