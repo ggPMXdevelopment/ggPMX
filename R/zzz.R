b1a0ce53be7763274903cb2835084708b72f67ce
gasseam1 <amine.gassem@ag-study.com>
2017-05-28 15:05:50 +0200

    enhance default parameters when loading package for the first time

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,11 @@
 .onLoad <- function(libname = find.package("ggPMX"), pkgname = "ggPMX"){
 
-  pmxOptions(work_dir = "/home/agstudy")
-
   pmxOptions(template_dir = file.path(libname, pkgname, "templates"))
+  theophylline <- file.path(libname, pkgname,"testdata", "theophylline")
+  WORK_DIR <- file.path(theophylline, "Monolix")
+  input_file <- file.path(theophylline, "data_pk.csv")
+  pmxOptions(work_dir = WORK_DIR, input = input_file, dv = "Y")
+  
 
 
 }
