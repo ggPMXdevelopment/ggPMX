55be96ef3c186c78194748665841bc0de2b979b2
gasseam1 <amine.gassem@ag-study.com>
2017-08-28 16:22:14 +0200

    add unit test #58

--- tests/testthat/test-shrinkage.R
+++ tests/testthat/test-shrinkage.R
@@ -15,3 +15,17 @@ test_that("test shrinkage for standing config", {
 
 })
 
+
+
+test_that("test shrinkage fun parameter", {
+  
+  ctr <- pmxClassHelpers$ctr
+  expect_is(ctr, "pmxClass")
+  est_ <- ctr %>% get_data("estimates")
+  eta_ <- ctr %>% get_data("eta")
+  res.var <- shrinkage(est_,eta_,fun="var")
+  res.sd <- shrinkage(est_,eta_)
+  expect_true(all(res.var$SHRINK > res.sd$SHRINK))
+
+})
+
