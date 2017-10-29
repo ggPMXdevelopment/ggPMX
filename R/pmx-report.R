2eec2632e51ab80b0b4d42d521ee37ce9f0da4bf
gasseam1 <amine.gassem@ag-study.com>
2017-10-29 23:09:35 +0100

    add report example using 1_pop_Pk_model

--- R/pmx-report.R
+++ R/pmx-report.R
@@ -10,8 +10,26 @@
 #'
 #' @examples
 #' \dontrun{
-#' pmx_report("my_report")
+#' c.name <-  "1_popPK_model"
+#' data_file <-  "PKdata_ggPMX.csv"
+#' uc.dir <- system.file(package = "ggPMX", "testdata",uc.name)
+#' wd.mlx <- file.path(uc.dir, "Monolix")
+#' input_file <- file.path(uc.dir, data_file)
+#' params <- 
+#' list(
+#' config="standing",
+#' directory = wd.mlx, 
+#' input = input_file, 
+#' dv = "DV", 
+#' dvid = "ytype", 
+#' cats = c("SEX","RACE","DISE","ILOW"), 
+#' conts = c("AGE0","WT0","HT0","TRT"),
+#' occ="ISS"
+#' )
+#' ctr <- do.call(pmx_mlx,params)
+#' ctr %>% pmx_report("1_popPK_model","all")
 #' }
+#' 
 pmx_report <-
   function(ctr,name,template="standing",render=TRUE){
     file_name <- sprintf("%s.Rmd",name)
