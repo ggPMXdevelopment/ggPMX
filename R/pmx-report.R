11699b433a85fee90782ade22c9faf0dcce59631
gasseam1 <amine.gassem@ag-study.com>
2017-11-09 12:07:30 +0100

    enhance pmx_report doc

--- R/pmx-report.R
+++ R/pmx-report.R
@@ -1,4 +1,4 @@
-#' Creates a standing report
+#' Creates a pdf document from a pre-defined template
 #'
 #' @param name \code{character} report name
 #' @param template \code{character} report template
@@ -8,14 +8,21 @@
 #' (defaults to the directory of the input file).
 #' @param ctr controller
 #'
-#' @return a new folder containg standing report template
 #' @export
 #' @importFrom rmarkdown draft render
-#'
-#' @example inst/examples/report.R
-#' 
+#' @details 
+#' \code{pmx_report} uses pre-defined template .Rmd to generate the report. 
+#' The idea is to pass the controller as a report argument using knitr \code{params} artifact. 
+#' If render=TRUE , it will generate the pdf report and the template used to create that report. 
+#' The user can choose to store both in output_dir. 
+#' @examples 
+#' \dontrun{
+#' ctr <- pk_occ() 
+#' ctr %>% pmx_report(name="1_popPK_model",template = "all")
+#' ctr %>% pmx_report(name="1_popPK_model",template = "standing",output_dir="/tmp")
+#' }
 pmx_report <-
-  function(ctr, name, output_dir=NULL,template="standing", render=TRUE) {
+  function(ctr, name, output_dir=NULL,template="all", render=TRUE) {
     
     file_name <- file.path(output_dir,sprintf("%s.Rmd", name))
     if (file.exists(file_name)) {
