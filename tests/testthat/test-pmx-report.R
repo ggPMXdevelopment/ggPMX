daa887bd707814b6d9944ad31440fe8ce820df9a
tynesjo <tynesjo@users.noreply.github.com>
2021-02-03 13:27:34 +0000

    Individual plot legend.

---
+++ tests/testthat/test-pmx-report.R
@@ -0,0 +1,19 @@
+context("Test generation of report from a pre-defined template")
+pmxClassHelpers <- test_pmxClass_helpers()
+
+test_that("can generate report", {
+  ctr <- pmxClassHelpers$ctr
+  expect_is(ctr, "pmxClass")
+  tmp_dir <- tempdir(check=TRUE)
+
+  pmx_report(
+    contr=ctr,
+    name="Report_ggPMX",
+    save_dir=tmp_dir,
+    extension="all",
+    format="both"
+  )
+
+  list_of_rep <- list.files(path=tmp_dir,pattern = "Report_ggPMX\\..*")
+  expect_equal(length(list_of_rep), 4L)
+})
