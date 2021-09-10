1b8502c1693dbb6d51f927e34ef1fab4eec3d83e
Johannes Tynes <j0@tynes.science>
2021-09-10 15:48:08 +0000

    Adding scientific notation and rounding options for param_table.

--- R/param_table.R
+++ R/param_table.R
@@ -3,7 +3,8 @@
 #' @param ctr Generated controller from e.g. \code{\link{pmx_mlx}} for Monolix.
 #' @param return_table If TRUE, returns the same table as in get_data('estimates') otherwise it returns a kable
 #' @param fun \code{character} can be "sd" or "var" for shrinkage computation, see \code{\link{pmx_comp_shrink}}
-#'
+#' @param scientific \code{logical} set to TRUE to get scientific notation of parameter values, or FALSE otherwise
+#' @param digits \code{integer} the number of significant digits to use when rounding parameter values
 #' @return Returns a kable with the parameter estimates from get_data('estimates')
 #' @export
 #'
@@ -11,127 +12,113 @@
 #' #ctr <- theophylline()
 #' #my_params <- ctr %>% param_table(fun = "var")
 
-param_table <- function(ctr,fun,return_table = FALSE) {
+param_table <- function(ctr, fun, return_table=FALSE, scientific=TRUE, digits=2) {
+
+  # Argument Checks
+  if(missing(fun)) fun <- c("var","sd")
+  stopifnot(
+    is.character(fun),
+    fun %in% c("sd", "var") | identical(fun, c("var", "sd")),
+    is.logical(return_table),
+    length(return_table) == 1,
+    is.logical(scientific),
+    length(scientific) == 1,
+    is.numeric(digits),
+    length(digits) == 1,
+    digits >= 0
+  )
+
+  if(ctr[["config"]][["sys"]] == "nlmixr"){
+    message("`param_table()` is not yet implemented for nlmixr")
+    return(NULL)
+  }
 
   ## Avoid error message in cmd check
-  
+
   SE <- EFFECT <- SHRINK <- RSE <- VALUE <- PARAM <- . <- NULL
-  
-  
-  pop_pars = ctr %>% get_data('estimates')
+
+  pop_pars <- ctr %>% get_data("estimates")
   rownames(pop_pars) <- trimws(as.character(pop_pars[["PARAM"]]))
-  
-  if(ctr$config$sys == "mlx" | ctr$config$sys == "mlx18" ){
-    
-    if(missing(fun)) fun <- c("var","sd")
-    
-    if(identical(fun,c("var","sd"))){
-      fun = "var"
-      msg <- "`var` was used for shrinkage calculation"
-      message(msg)
-    } else if (fun == "var") {
-      msg <- "`var` was used for shrinkage calculation"
-      message(msg)
-    } else if (fun == "sd") {
-      msg <- "`sd` was used for shrinkage calculation"
-      message(msg)
-    }
-    
-    shrinkage = ctr %>% pmx_comp_shrink(fun=fun) %>% 
-      dplyr::mutate(SHRINK=paste0(round(SHRINK*100),'%')) %>% # make % shrinkage
-      dplyr::mutate(PARAM=paste0('omega_',EFFECT)) %>%  #generate new column PARAM with "omega_" prefix
-      dplyr::select(PARAM,SHRINK) %>% #select PARAM and SHRINK (%)
-      dplyr::mutate(PARAM = as.character(PARAM))
-    
-    pop_pars2 = pop_pars %>% 
-      dplyr::mutate(VALUE=as.character(signif(VALUE,2)),RSE=ifelse(is.na(RSE), 'fixed',paste0(round(RSE),'%'))) %>% #make RSE in %, and NaN to "fixed"
-      dplyr::select(PARAM,VALUE,RSE) %>% #select param and value
-      dplyr::mutate(PARAM = as.character(PARAM)) %>% #set param as character
-      dplyr::arrange(PARAM) %>% #sort according to param
-      dplyr::mutate(PARAM = trimws(PARAM)) %>% #remove white space
-      dplyr::left_join(shrinkage, by = "PARAM") %>% #join shrinkage
-      dplyr::mutate(SHRINK=ifelse(is.na(SHRINK),'',SHRINK)) # remove NAs from empty shrink
-    
-    pop_pars3 = suppressWarnings(  dplyr::bind_rows(data.frame(PARAM='.',VALUE='', RSE ='', SHRINK='')) %>%
-                                   dplyr::bind_rows(pop_pars2 %>% dplyr::filter(grepl('_pop',PARAM))) %>% 
-                                   dplyr::bind_rows(data.frame(PARAM='.',VALUE='', RSE ='', SHRINK='')) %>%
-                                   dplyr::bind_rows(pop_pars2 %>% dplyr::filter(grepl('omega_',PARAM))) %>% 
-                                   dplyr::bind_rows(data.frame(PARAM='.',VALUE='', RSE ='', SHRINK='')) %>%
-                                   dplyr::bind_rows(pop_pars2 %>% dplyr::filter(grepl('beta_',PARAM))) %>% 
-                                   dplyr::bind_rows(data.frame(PARAM='.',VALUE='',RSE ='', SHRINK='')) %>%
-                                   dplyr::bind_rows(pop_pars2 %>% dplyr::filter(PARAM %in% c('a','b'))) %>% 
-                                   dplyr::bind_rows(data.frame(PARAM='.',VALUE='',RSE ='', SHRINK=paste("comp. with",fun))) %>% 
-                                   dplyr::rename(Parameter=PARAM, Estimate=VALUE, Relative_SE=RSE,Shrinkage=SHRINK))
-  }
-  
-  if(ctr$config$sys == "nm"){ 
-    
-    #currently the shrinkage is calulcated using sd for nonmem, pmx_comp_shrink uses values from get_data("omega") which is the "sd" for nonmem
-    if(missing(fun)) fun <- c("var","sd")
-    
-    if(identical(fun,c("var","sd"))){
-      fun = "sd"
-      msg <- "`sd` was used for shrinkage calculation"
-      message(msg)
-    } else if (fun == "var") {
-      msg <- "`var` was used for shrinkage calculation"
-      message(msg)
-    } else if (fun == "sd") {
-      msg <- "`sd` was used for shrinkage calculation"
-      message(msg)
-    }
-    
-    shrinkage = ctr %>% pmx_comp_shrink(fun = fun) %>% #because for nonmem data is used of get_data("omega"), which is the SD
-      dplyr::mutate(SHRINK=paste0(round(SHRINK*100),'%')) %>% # make % shrinkage
-      dplyr::mutate(PARAM=EFFECT) %>%  #generate new column PARAM with "omega_" prefix
-      dplyr::select(PARAM,SHRINK) %>% #select PARAM and SHRINK (%)
-      dplyr::mutate(PARAM = as.character(PARAM))
-    
-    i = 1
-    for(i in i:length(shrinkage$PARAM)) {
-      str <- shrinkage$PARAM[i]
-      num <- as.numeric(substring(str,4,))
-      shrinkage$PARAM[i] <- paste0("OMEGA(",num,",",num,")")
+
+  if(return_table) {return(as.data.table(pop_pars))}
+
+  config_sys_nm <- ctr[["config"]][["sys"]] == "nm"
+
+
+  # Selecting function to use for shrinkage calculation
+  fun <- ifelse(
+    identical(fun, c("var", "sd")),
+    ifelse(config_sys_nm, "sd", "var"),
+    fun
+  )
+
+  message("`", fun, "`",  " was used for shrinkage calculation")
+
+  # For nonmem data is used of get_data("omega"), which is the SD
+  shrinkage <- ctr %>% pmx_comp_shrink(fun=fun) %>%
+    dplyr::transmute(
+      # make % shrinkage
+      SHRINK=paste0(round(SHRINK * 100), "%"),
+      # For non-nm sys PARAM has "omega_" prefix
+      PARAM=paste0(ifelse(config_sys_nm, "", "omega_"), EFFECT),
+    )
+
+  if(ctr[["config"]][["sys"]] == "nm") {
+    i <- 1
+    for(i in i:length(shrinkage[["PARAM"]])) {
+      str <- shrinkage[["PARAM"]][i]
+      num <- as.numeric(substring(str, 4, ))
+      shrinkage[["PARAM"]][i] <- paste0("OMEGA(", num, ",", num, ")")
     }
 
-    pop_pars2 = pop_pars %>% 
-      dplyr::mutate(RSE=round(RSE)) %>% 
-      dplyr::mutate(RSE=ifelse(SE == 1E10, "fixed", RSE)) %>% 
-      dplyr::mutate(VALUE=as.character(signif(VALUE,2)),RSE=ifelse(RSE != "fixed",paste0(RSE,'%'), RSE)) %>%
-      dplyr::select(PARAM,VALUE,RSE) %>% #select param and value
-      dplyr::mutate(PARAM = as.character(PARAM)) %>% #set param as character
-      dplyr::arrange(PARAM) %>% #sort according to param
-      dplyr::mutate(PARAM = trimws(PARAM)) %>% #remove white space
-      dplyr::left_join(shrinkage, by = "PARAM") %>% #join shrinkage
-      dplyr::mutate(SHRINK=ifelse(is.na(SHRINK),'',SHRINK)) # remove NAs from empty shrink
+    pop_pars <- dplyr::mutate(pop_pars, RSE, ifelse(SE == 1E10, "fixed", round(RSE)))
+  }
 
-    pop_pars3 =  suppressWarnings(dplyr::bind_rows(pop_pars2 %>% dplyr::filter(grepl('OBJ',PARAM))) %>% 
-                                   dplyr::bind_rows(data.frame(PARAM='.',VALUE='', RSE ='', SHRINK='')) %>%
-                                   dplyr::bind_rows(pop_pars2 %>% dplyr::filter(grepl('THETA',PARAM))) %>% 
-                                   dplyr::bind_rows(data.frame(PARAM='.',VALUE='', RSE ='', SHRINK='')) %>%
-                                   dplyr::bind_rows(pop_pars2 %>% dplyr::filter(grepl('SIGMA',PARAM))) %>% 
-                                   dplyr::bind_rows(data.frame(PARAM='.',VALUE='', RSE ='', SHRINK='')) %>%
-                                   dplyr::bind_rows(pop_pars2 %>% dplyr::filter(grepl('OMEGA',PARAM))) %>%
-                                   dplyr::bind_rows(data.frame(PARAM='.',VALUE='',RSE ='', SHRINK=paste("comp. with",fun))) %>% 
-                                   dplyr::rename(Parameter=PARAM, Estimate=VALUE, Relative_SE=RSE,Shrinkage=SHRINK))
-  }
-  
-  if(ctr$config$sys == "nlmixr"){
-    message("`param_table()` is not yet implemented for nlmixr")
-  }
-  
-  if(return_table) {
-    pop_pars3 <- as.data.table(pop_pars)
-    return_kable <- FALSE
-  } else {
-    return_kable <- TRUE
-  }
-  
-  if(return_kable) {
-    pop_pars3 <- pop_pars3 %>% knitr::kable(col.names = c("Parameter", "Value", "Relative Standard Error","Shrinkage"), 
-                                            caption=paste0('Parameter estimates (',nrow(pop_pars3)+1,' lines)'),align='lrrr')
-  }
-    return(pop_pars3)
+  blank_row <- data.frame(PARAM=".", VALUE="", RSE ="", SHRINK="")
+
+  pop_pars %>%
+  dplyr::transmute(
+    PARAM=trimws(PARAM),
+    VALUE=signif(VALUE, digits) %>% {
+      if(scientific) {scales::label_scientific(digits=Inf)(.)}
+      else {as.character(.)}
+    },
+    # Make RSE a %, and change NaN to "fixed"
+    RSE=ifelse(is.na(RSE), "fixed", paste0(round(RSE), "%")),
+  ) %>%
+  dplyr::arrange(PARAM) %>%
+  dplyr::left_join(shrinkage, by="PARAM") %>%
+  tidyr::replace_na(list(SHRINK="")) %>%
+  {suppressWarnings({
+    if(ctr[["config"]][["sys"]] %in% c("mlx", "mlx18" )) {
+      dplyr::bind_rows(
+        blank_row,
+        dplyr::filter(., grepl("_pop", PARAM)),
+        blank_row,
+        dplyr::filter(., grepl("omega_", PARAM)),
+        blank_row,
+        dplyr::filter(., grepl("beta_", PARAM)),
+        blank_row,
+        dplyr::filter(., PARAM %in% c("a", "b")),
+     )
+    } else if(ctr[["config"]][["sys"]] == "nm") {
+      dplyr::bind_rows(
+        dplyr::filter(., grepl("OBJ", PARAM)),
+        blank_row,
+        dplyr::filter(., grepl("THETA", PARAM)),
+        blank_row,
+        dplyr::filter(., grepl("SIGMA", PARAM)),
+        blank_row,
+        dplyr::filter(., grepl("OMEGA", PARAM)),
+      )
+    }
+  })} %>%
+  dplyr::bind_rows(data.frame(PARAM=".",VALUE="",RSE ="", SHRINK=paste("comp. with", fun))) %>%
+  dplyr::rename(Parameter=PARAM, Estimate=VALUE, Relative_SE=RSE, Shrinkage=SHRINK) %>%
+  knitr::kable(
+    x=.,
+    col.names=c("Parameter", "Value", "Relative Standard Error", "Shrinkage"),
+    caption=paste0("Parameter estimates (", nrow(pop_pars) + 1, " lines)"),
+    align="lrrr"
+  )
 }
-
-
--- tests/testthat/test-param_table.R
+++ tests/testthat/test-param_table.R
@@ -1,32 +1,48 @@
 context("Test param_table()")
 
 test_that("can make param_table()", {
-  
+
+  # Creating "pmxClass" controllers:
   ctr <- theophylline()
-  
-  nonmem_dir <- file.path(system.file(package = "ggPMX"), "testdata","extdata")
-  ctr_nm <- pmx_nm(directory = nonmem_dir, runno = "001")
-  
-  p_ctr <- param_table(ctr)
-  
-  p_ctr_nm <- param_table(ctr_nm)
-  
-  #check headers of param_table
+
+  ctr_nm <- pmx_nm(
+    directory=file.path(system.file(package = "ggPMX"), "testdata","extdata"),
+    runno="001"
+  )
+
+  # Creating kable outputs for testing:
+  p_ctr <- param_table(ctr, digits=2, scientific=FALSE)
+  p_ctr_nm <- param_table(ctr_nm, digits=2, scientific=FALSE)
+  p_ctr_sci <- param_table(ctr, digits=2, scientific=TRUE)
+
+  # Check headers
   expect_true(
     "|Parameter      |  Value| Relative Standard Error|      Shrinkage|" %in% trimws(p_ctr)
   )
-  
+
   expect_true(
     "|Parameter  |  Value| Relative Standard Error|     Shrinkage|" %in% trimws(p_ctr_nm)
   )
-  
+
   #check a random row (here 5) of param_table
   expect_true(
-    "|Cl_pop         |   0.31|                      8%|               |" %in% trimws(p_ctr), 
+    "|Cl_pop         |   0.31|                      8%|               |" %in% trimws(p_ctr),
   )
 
   expect_true(
-    "|THETA1     |     26|                      3%|              |" %in% trimws(p_ctr_nm),  
+    "|THETA1     |     26|                      3%|              |" %in% trimws(p_ctr_nm),
   )
-  
+
+  # Check class:
+  expect_s3_class(p_ctr, "knitr_kable")
+  expect_s3_class(p_ctr_nm, "knitr_kable")
+  # Check output lengths:
+  expect_length(p_ctr, 23L)
+  expect_length(p_ctr_nm, 23L)
+  expect_length(p_ctr_sci, 23L)
+
+  # Check scientific notation:
+  p_ctr_sci <- param_table(ctr, digits=2, scientific=TRUE)
+  expect_true(any(grepl("\\de\\+\\d", p_ctr_sci)))
+  expect_true(any(grepl("\\de\\-\\d", p_ctr_sci)))
 })
