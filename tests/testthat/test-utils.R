524d54092e40c6d154ce7b318986644c3b61e987
tynsci <90842492+tynsci@users.noreply.github.com>
2022-05-13 08:23:50 +0000

    Tests of parse_mlxtran functionality

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -28,3 +28,74 @@ test_that("pk_pd is worrking", {
   ctr <- pk_pd()
   expect_s3_class(ctr, "pmxClass")
 })
+
+
+## Testing parse_mlxtran below
+
+file_name <- file.path(system.file(package = "ggPMX"),
+                       "testdata",
+                       "1_popPK_model",
+                       "project.mlxtran")
+
+wd <- file.path(system.file(package = "ggPMX"),
+                "testdata",
+                "1_popPK_model")
+
+mlxpath <- file.path(wd, "project_copy.mlxtran")
+
+test_that("parse_mlxtran: params: folder name", {
+  a <- parse_mlxtran(file_name)
+  expect_true(inherits(
+    a,
+    "list"
+  ))
+  expect_true(a$directory == file.path(wd, "RESULTS"))
+})
+
+test_that("parse_mlxtran: params: full file_name", {
+  dir.create(file.path(wd, "result"))
+  section.name <- line <- section <- NULL
+  sub_section <- sub_section.name <- NULL
+  value <- NULL
+  lines <- readLines(file_name)
+
+  firsts <- min(grep("<.*>", lines)) # first section
+  lines <- lines[firsts:length(lines)]
+  lines <- lines[lines != "" & !grepl(":$", lines)]
+  lines[grepl("exportpath = ", lines) == TRUE] <- paste0("exportpath = '", wd, "/result'")
+
+  writeLines(lines, mlxpath)
+  a <- parse_mlxtran(mlxpath)
+  file.remove(mlxpath)
+  unlink(file.path(wd, "result"), recursive = TRUE)
+
+  expect_true(inherits(
+    a,
+    "list"
+  ))
+  expect_true(a$directory == file.path(wd, "result"))
+})
+
+test_that("parse_mlxtran: params: no exist file_name", {
+  dir.create(file.path(wd, "result"))
+  section.name <- line <- section <- NULL
+  sub_section <- sub_section.name <- NULL
+  value <- NULL
+  lines <- readLines(file_name)
+
+  firsts <- min(grep("<.*>", lines)) # first section
+  lines <- lines[firsts:length(lines)]
+  lines <- lines[lines != "" & !grepl(":$", lines)]
+  lines[grepl("exportpath = ", lines) == TRUE] <- paste0("exportpath = '", wd, "/result/res'")
+
+  writeLines(lines, mlxpath)
+  a <- parse_mlxtran(mlxpath)
+  file.remove(mlxpath)
+  unlink(file.path(wd, "result"), recursive = TRUE)
+
+  expect_true(inherits(
+    a,
+    "list"
+  ))
+  expect_true(a$directory == file.path(wd, "RESULTS"))
+})
