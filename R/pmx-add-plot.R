af4b334e64043ab4d450d27848b92502679f12bd
tynsci <90842492+tynsci@users.noreply.github.com>
2022-10-06 09:43:38 +0000

    Modifying code to handle strat.facet case and adding test

--- R/pmx-add-plot.R
+++ R/pmx-add-plot.R
@@ -81,17 +81,15 @@ before_add_check <- function(self, private, x, pname) {
     gp[["labels"]][["legend"]] <- x[["strat.color"]]
     x[["gp"]] <- gp
   }
-  if (!is.null(x[["strat.facet"]])) {
-    tit <- x$gp[["labels"]][["title"]]
-    tit <- gsub(" by .*", "", tit)
-    if ((isTRUE(x$gp[["custom_title"]])) && (x$gp[["custom_title"]] == FALSE)) {
-      x$gp[["labels"]][["title"]] <- sprintf("%s by %s",
-        tit, formula_to_text(x[["strat.facet"]]))
-    }
-  }
-  else {
-    if ((isTRUE(x$gp[["custom_title"]])) && (x$gp[["custom_title"]] == FALSE)) {
-      x$gp[["labels"]][["title"]] <- gsub(" by.*", "", x$gp[["labels"]][["title"]])
+
+  if (isFALSE(x[["gp"]][["custom_title"]])) {
+    x[["gp"]][["labels"]][["title"]] <- gsub(" by .*", "", x[["gp"]][["labels"]][["title"]])
+    if (!is.null(x[["strat.facet"]])) {
+      x[["gp"]][["labels"]][["title"]] <- sprintf(
+          "%s by %s",
+          x[["gp"]][["labels"]][["title"]],
+          formula_to_text(x[["strat.facet"]])
+        )
     }
   }
   invisible(x)
