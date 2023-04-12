7148d302c0723048ee9e23be4e631cf60944e413
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-04-12 00:17:33 -0500

    Use reframe().  Closes #353

--- tests/testthat/test-nlmixr.R
+++ tests/testthat/test-nlmixr.R
@@ -125,7 +125,7 @@ test_that("integrated demo", {
     lapply(doses, function(x) {
       ids <- dat %>%
         dplyr::filter(DOSE == x) %>%
-        dplyr::summarize(ids = unique(ID)) %>%
+        dplyr::reframe(ids = unique(ID)) %>%
         dplyr::pull()
       ids <- ids[seq(1, nid)]
       dat %>%
