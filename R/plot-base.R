4baa3fce31b2d24f7d31848b143a5090746782e1
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-07-28 12:46:51 -0500

    Fixes

--- R/plot-base.R
+++ R/plot-base.R
@@ -1,10 +1,3 @@
-#' @export
-plot_pmx.pmx_gpar <- function(x, dx, ...) {
-  extra <- list(...)
-  if (length(extra != 1)) stop("plot_pmx for pmx_gpar needs exactly 3 arguments", call.=FALSE)
-  plot_pmx_gpar_real(gpar=x, dx=p, bloq_cens=extra[[1]])
-}
-
 #' The ggPMX base plot function
 #'
 #'
@@ -13,13 +6,15 @@ plot_pmx.pmx_gpar <- function(x, dx, ...) {
 #' @param gpar object of pmx_gpar type
 #' @param p plot
 #' @param bloq_cens bloq censored column name
+#' @param ... ignored parameters
 #' @import ggplot2
 #' @family plot_pmx
 #' @return ggplot2 object
 #' @export
-plot_pmx_gpar_real <- function(gpar, p, bloq_cens) {
+plot_pmx.pmx_gpar <- function(gpar, p, bloq_cens, ...) {
   assert_that(is_pmx_gpar(gpar))
   assert_that(is_ggplot(p))
+  if (length(list(...)) != 0) stop("plot_pmx.pmx_gpar requires 3 arguments", call.=FALSE)
   with(gpar, {
     assert_that(is_list_or_null(smooth))
     assert_that(is_list_or_null(band))
